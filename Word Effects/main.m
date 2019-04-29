28b1b851e9a4938a6b4efa2ebb32856e771ac9ac
Frank Chen <frankcrestview@hotmail.com>
2019-04-29 12:02:55 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Frank Chen on 2019-04-29.
+//  Copyright © 2019 Frank Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
