6bf70ac47ecc494881a47fae2f3769ae6ac34660
Frank Chen <frankcrestview@hotmail.com>
2019-04-29 12:58:09 -0400

    first commit

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -10,8 +10,35 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        while (YES) {
+
+        // 255 unit long array of characters
+        char inputChars[255];
+        char inputNumber[255];
+        
+        printf("Input a string: ");
+        // limit input to max 255 characters
+        fgets(inputChars, 255, stdin);
+        NSLog(@"%s",inputChars);
+        
+        // print as a c string
+        printf("Your string is %s\n", inputChars);
+            
+        printf("Input a number:");
+        fgets(inputNumber, 255, stdin);
+        NSLog(@"%s", inputNumber);
+            
+        printf("Your string is %s\n", inputNumber);
+            
+        // convert char array to an NSString object
+        NSString *inputString = [NSString stringWithUTF8String:inputChars];
+        NSString *inputNumberString = [NSString stringWithUTF8String:inputNumber];
+        // print NSString object
+        NSLog(@"Input was: %@", inputString);
+            NSLog(@"Input was: %@", inputNumberString);
+        }
+        
     }
     return 0;
 }
